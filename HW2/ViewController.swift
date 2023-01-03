e3655ddb54f395558259377bc6bb71a9124f7040
KarinaChadaeva <118630641+KarinaChadaeva@users.noreply.github.com>
2023-01-03 19:49:29 +0300

    The main version of my app (the main task of my homework) is ready

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
     }
 
-
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender.tag {
+        case 0: blueLabel.text = string(from: sender)
+        case 1: greenLabel.text = string(from: sender)
+        case 2: redLabel.text = string(from: sender)
+        default: break
+        }
+    }
+    
+    private func setColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
+                                           green: CGFloat(greenSlider.value) / 255,
+                                           blue: CGFloat(blueSlider.value) / 255,
+                                           alpha: 1)
+    }
+    
+    @IBAction func setValue() {
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(Int(slider.value))
+    }
+    
 }
 
